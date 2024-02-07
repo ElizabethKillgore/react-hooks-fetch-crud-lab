e7208a89cd918ee57ba7552bbf4c3f0d34909298
Elizabeth <ek@elizabethkillgore.com>
2024-02-07 16:24:54 -0500

    react hooks fetch crud lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, allQuestions, onDeleteQuestion, onUpdateAnswer }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,41 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+function handleDeleteClick() {
+  fetch(`http://localhost:4000/questions/${question.id}`, {
+    method: "DELETE",
+  })
+    .then((r) => r.json())
+    .then(()=> onDeleteQuestion(question))
+}
+
+function handleUpdateAnswerChange(event) {
+  console.log(event.target.value)
+
+  fetch(`http://localhost:4000/questions/${question.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application:json",
+        },
+        body: JSON.stringify({correctIndex: parseInt(event.target.value)})
+
+  })
+        .then((r) => r.json())
+        .then((updatedAnswer) => onUpdateAnswer(updatedAnswer))
+   }
+
+
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleUpdateAnswerChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button className="remove" onClick={handleDeleteClick}>Delete Question</button>
+
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import QuestionItem from "./QuestionItem"
 
-function QuestionList() {
+function QuestionList( { questions, onDeleteQuestion, onUpdateAnswer  }) {
+  
+  let allQuestions = questions.map((question) => {
+    return  <QuestionItem  key={question.id} question={question} onDeleteQuestion={onDeleteQuestion} onUpdateAnswer={onUpdateAnswer}/> 
+  }
+)
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul className="Questions"> {allQuestions} 
+        
+      </ul> 
     </section>
   );
 }
 
 export default QuestionList;
+
+
+ 
+ 
